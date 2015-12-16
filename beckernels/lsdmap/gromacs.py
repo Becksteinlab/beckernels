fd8a68b2971515cd6b0bfcdb495240403d6392c4
David Dotson <dotsdl@gmail.com>
2015-12-15 22:52:17 -0700

    Added use_build function to lsdmap/gromacs.
    Since we have multiple versions available on, say, stampede, this
    function gives us back a Kernel with the version we want.

--- beckernels/lsdmap/gromacs.py
+++ beckernels/lsdmap/gromacs.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+"""Required kernel for the run component of LSDmap directed MD. This mostly
+just sets up the environment, then runs `run.py` which generates the script
+that then executes the actual MD run.
+
+Credit to Vivek Balasubramanian for the original content this is based on.
+
+"""
+
 __author__    = "David Dotson <dotsdl@gmail.com>"
 __license__   = "MIT"
 
@@ -7,36 +15,41 @@ from radical.ensemblemd.exceptions import NoKernelConfigurationError
 from radical.ensemblemd.kernel_plugins.kernel_base import KernelBase
 
 _KERNEL_INFO = {
-    "name":         "beckstein.gromacs-5_1_1",
+    "name":         "beckstein.gromacs",
     "description":  "A kernel for executing gromacs jobs on remote clusters using Beckstein Lab builds.",
-    "arguments":   {"--grompp=":
+    "arguments":   {
+                        "--grompp=":
                         {
                             "mandatory": True,
                             "description": "Input parameter filename"
                         },
-                    "--topol=":
+                        "--topol=":
                         {
                             "mandatory": True,
                             "description": "Topology filename"
                         }
-                    },
-    "machine_configs": {
-        "xsede.stampede":
+                    }
+                }
+
+
+gmx_versions = {
+        '5.1.1':
         {
-            "environment" : {},
-            "pre_exec" : ["module purge",
-                          "module load gcc/4.9.1",
-                          "module load mvapich2/2.1",
-                          "module load python",
-                          "source /home1/02142/dldotson/Library/gromacs/versions/gromacs-5.1.1/gcc-4.9.1/mvapich2-2.1/fftw3-3.3.4/cuda-6.5/bin/GMXRC.bash"],
-            "executable" : ["python"],
-            "uses_mpi"   : True
-        },
-
-    }
-}
-
-gmx_versions = 
+            "machine_configs": 
+            {
+                "xsede.stampede":
+                {
+                    "environment" : {},
+                    "pre_exec" : ["module purge",
+                                  "module load gcc/4.9.1",
+                                  "module load mvapich2/2.1",
+                                  "module load python",
+                                  "source /home1/02142/dldotson/Library/gromacs/versions/gromacs-5.1.1/gcc-4.9.1/mvapich2-2.1/fftw3-3.3.4/cuda-6.5/bin/GMXRC.bash"],
+                    "executable" : ["python"],
+                    "uses_mpi"   : True
+                }
+            }
+        }
 
 
 def use_build(gmx_version):
@@ -48,13 +61,19 @@ def use_build(gmx_version):
     gmx_version : string
         string giving the version, e.g. '5.1.1' of gromacs to use
 
+    Returns
+    -------
+    kernel
+        Kernel with the appropriate parameters for the selected gmx version
+
     """
+    # we need to deal in copies
+    out_schema = dict(_KERNEL_INFO)
+    out_schema.update(dict(gmx_versions[gmx_version]))
+    return Kernel(out_schema)
     
 
 class Kernel(KernelBase):
-
-    def __init__(self):
-        super(Kernel, self).__init__(_KERNEL_INFO)
 
     def _bind_to_resource(self, resource_key):
         if resource_key not in _KERNEL_INFO["machine_configs"]:
