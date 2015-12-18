9a0449548e3ad10f788ec8147e50356e5fc7fca8
David Dotson <dotsdl@gmail.com>
2015-12-18 10:51:00 -0700

    RE-added registration.

--- beckernels/__init__.py
+++ beckernels/__init__.py
@@ -1,3 +1,15 @@
 """A library of `radical.ensemblemd` kernels specific to Beckstein Lab resources.
 
 """
+from radical.ensemblemd.engine.kernel_registry import kernel_registry
+
+# register our kernels with radical.ensemblemd so that calling by name in
+# Kernel constructor works
+
+_kernels = ['beckernels.dmdmd.gromacs',
+            'beckernels.dmdmd.lsdmap',
+            'beckernels.dmdmd.pre_grlsd_loop',
+            'beckernels.dmdmd.pre_lsdmap',
+            'beckernels.dmdmd.post_lsdmap']
+
+kernel_registry.extend(_kernels)
